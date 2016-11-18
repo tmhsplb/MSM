1558db2083f4d695a69966f19d7dfe0c1f8fe6f0
tmhsplb <peter3418@ymail.com>
2016-11-18 14:41:37 -0600

    Set desktop = false on app.js

--- MSM/app.js
+++ MSM/app.js
@@ -1,5 +1,5 @@
 ﻿
-var desktop = true;
+var desktop = false;
 
 var FileServices = angular.module('FileServices', ['ngResource']);
 var MergeServices = angular.module('MergeServices', ['ngResource']);
